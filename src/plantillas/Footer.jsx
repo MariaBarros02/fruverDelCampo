25515fd54784a082b1caaa949f299a259603f844
MariaBarros02 <141078532+MariaBarros02@users.noreply.github.com>
2024-10-23 12:04:57 -0500

    Actualizacion de variables y componentes Se han establecido variables de entorno, realizado ajustes de color y aplicado las variables creadas.  Sobre la rama _main 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/plantillas/Footer.jsx 	modified:   src/sass/base/_configuracion.scss 	modified:   src/sass/base/_variables.scss 	modified:   src/sass/componentes/_heroPrincipal.scss 	modified:   src/sass/estilos.scss 	modified:   src/sass/plantillas/_cabecera.scss 	modified:   src/sass/plantillas/_footer.scss 	modified:   src/sass/plantillas/_navegacion.scss

--- src/plantillas/Footer.jsx
+++ src/plantillas/Footer.jsx
@@ -1,15 +1,16 @@
 import { FaInstagram, FaFacebook, FaWhatsapp } from "react-icons/fa";
 
+const yearActual = new Date().getFullYear(); 
+
 const Footer = () => {
-    const yearActual = new Date().getFullYear(); 
     return (
         <>
             <footer className="footer-contenedor">
                 <h3>Encuentranos en: </h3>
                 <div>
-                    <a href="https://www.instagram.com/" target="_blank"> <FaInstagram /></a>
-                    <a href="https://facebook.com/" target="_blank"><FaFacebook /></a>
-                    <a href="https://www.whatsapp.com/" target="_blank"><FaWhatsapp /></a>
+                    <a href="https://www.instagram.com/" target="_blank"> <FaInstagram color="#FF7A00" /></a>
+                    <a href="https://facebook.com/" target="_blank"><FaFacebook color="blue"/></a>
+                    <a href="https://www.whatsapp.com/" target="_blank"><FaWhatsapp color="green"/></a>
                 </div>
                 <p>fruverdelcampo@gmail.com | Tel: (608) 87554566</p>
                 <p>Carrera 16 #45 - 05 (Neiva, Huila)</p>
