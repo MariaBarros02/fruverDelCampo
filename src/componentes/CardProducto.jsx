7116ed2586ec4930f734889a8d0df04edd413ead
Joxxeph <josephguma0603@gmail.com>
2024-11-11 10:50:25 -0500

    Modal de productos Version 1
    Se realizo en su totalidad el modal para la
    realizacion de la compra de un producto, adecuado
    la informacion pertinente y los botones para
    cerrar el modal, aumentar lla cantdad de producto
    y pasar al carrito de compras.
     En la rama _joseph se cambio durante el commit:
    	modified:   "public/imagenes/productos/Lim\303\263n.webp"
    	modified:   src/componentes/CardProducto.jsx
    	modified:   src/sass/componentes/_cardProducto.scss

--- src/componentes/CardProducto.jsx
+++ src/componentes/CardProducto.jsx
@@ -4,9 +4,14 @@ import { Link } from "react-router-dom";
 
 const CardProducto = ({ producto }) => {
 
+  //modal-contenido
   const { ID, Producto, Complemento, Marca, Tamaño, Unidad, ÍtemsTiendas } = producto;
   const [mostrarModal, setMostrarModal] = useState(false)
 
+  // inputNumerico
+  const [valor, setValor] = useState(1);
+
+  //modal
   useEffect(() => {
     if (mostrarModal) {
       document.body.style.overflowY = 'hidden';
@@ -19,12 +24,31 @@ const CardProducto = ({ producto }) => {
     };
   }, [mostrarModal]);
 
+  //modal-contenido
   const abrirModal = () => {
     setMostrarModal(true);
   };
   const cerrarModal = () => {
     setMostrarModal(false)
   }
+
+  // inputNumerico
+  const incrementar = () => {
+    setValor(prev => prev + 1);
+  };
+
+  const decrementar = () => {
+    if (valor > 0) {
+      setValor(prev => prev - 1);
+    }
+  };
+
+  const manejarCambio = (e) => {
+    const nuevoValor = parseInt(e.target.value, 10);
+    if (!isNaN(nuevoValor)) {
+      setValor(nuevoValor)
+    }
+  };
 
   return (
     <>
@@ -41,13 +65,23 @@ const CardProducto = ({ producto }) => {
       {mostrarModal && (
         <div className="cardProducto-modal">
           <div className="cardProducto-modal_contenido">
-            <img src={`../imagenes/productos/${Producto}.webp`} alt={`imagen_${Producto}`} />
-            <div>
-            <p>ID: {ID}</p>
-            <p>{Producto} {Complemento}  x {Tamaño}{Unidad}</p>
-            <p>{formatearDinero(ÍtemsTiendas[0].Precio, 'COP')}</p>
-            <p className="cardProducto-marca">Marca: {Marca}</p>
-            <button onClick={cerrarModal}>Cerrar</button>
+            <div className="cardProducto-modal_cerrar"><button onClick={cerrarModal}>x</button></div>
+            <div className="cardProducto-modal_informacion">
+              <img src={`../imagenes/productos/${Producto}.webp`} alt={`imagen_${Producto}`} />
+              <div>
+                <p className="cardProducto-modal_id">ID: {ID}</p>
+                <p className="cardProducto-modal_nombre">{Producto} {Complemento}  x {Tamaño}{Unidad}</p>
+                <p className="cardProducto-modal_marca">Marca: {Marca}</p>
+                <p className="cardProducto-modal_precio">{formatearDinero(ÍtemsTiendas[0].Precio, 'COP')}</p>
+                <div className="cardProducto-modal_contenedorBtn">
+              <div className="inputNumerico">
+                <button onClick={decrementar}>-</button>
+                <input type="text" value={valor} onChange={manejarCambio} />
+                <button onClick={incrementar}>+</button>
+              </div>
+              <Link to="#" onClick={abrirModal} className="cardProducto-modal_anadirCarrito">Añadir al carrito</Link>
+              </div>
+            </div>
             </div>
           </div>
         </div>
