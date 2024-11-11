2ffa3213554e0b2c79826f445ad61fd083bf3ae7
Joxxeph <josephguma0603@gmail.com>
2024-11-11 17:26:07 -0500

    Categorias version 1
    Se realizo la seccion de categoria para la pagina
    Productos dejando pendiente el cambio en url de las
    categorias y el movimiento de la seccion con el
    scroll (este movimieto es "opcional")
    En la rama _joseph se cambio en este commit:
    modified:   src/paginas/Productos.jsx
    modified:   src/sass/componentes/_cardProducto.scss
    modified:   src/sass/paginas/_productos.scss

--- src/paginas/Productos.jsx
+++ src/paginas/Productos.jsx
@@ -3,18 +3,51 @@ import Cabecera from "../plantillas/Cabecera"
 import Footer from "../plantillas/Footer"
 import CardProducto from "../componentes/CardProducto"
 import { useState, useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom";
+
+const categoriasInfo = [
+  {
+    nombre: "Frutas",
+    url: "Categoría=frutas%20y%20verduras&Subcategoría=frutas",
+    totalPaginas: 6
+  },
+  {
+    nombre: "Verduras",
+    url: "Categoría=frutas%20y%20verduras&Subcategoría=verduras",
+    totalPaginas: 7
+  },
+  {
+    nombre: "Pulpas de fruta",
+    url: "Categoría=frutas%20y%20verduras&Subcategoría=pulpas%20de%20fruta",
+    totalPaginas: 6
+  },
+  {
+    nombre: "Carnes",
+    url: "Categoría=carnes%20y%20pescados&Subcategoría=res",
+    totalPaginas: 6
+  },
+  {
+    nombre: "Huevos",
+    url: "Categoría=despensa&Subcategoría=huevos",
+    totalPaginas: 3
+  }
+]
 
 const Productos = () => {
 
 
   const [productos, setProductos] = useState([]);
-  const [paginaActual, setPaginaActual] = useState(1)
-  const [totalPaginas, setTotalPaginas] = useState(6)
+  const [paginaActual, setPaginaActual] = useState(1);
+  const [categoria, setCategoria] = useState(categoriasInfo[0].nombre);
+  const [totalPaginas, setTotalPaginas] = useState(categoriasInfo[0].totalPaginas);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const obtenerProductos = async () => {
+      const categoriaSeleccionada = categoriasInfo.find(cat => cat.nombre === categoria);
 
-      const url = `https://losprecios.co/tienda/detalles?ID=6&MunicipioID=1&ClaveAPI=nfJrn941ba90fn2x&Categor%C3%ADa=frutas%20y%20verduras&P%C3%A1gina=${paginaActual}`
+
+      const url = `https://losprecios.co/tienda/detalles?ID=6&MunicipioID=1&ClaveAPI=nfJrn941ba90fn2x&${categoriaSeleccionada.url}&P%C3%A1gina=${paginaActual}`
       try {
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
@@ -26,40 +59,73 @@ const Productos = () => {
 
     };
     obtenerProductos();
-  }, [paginaActual]);
+  }, [paginaActual, categoria]);
 
-  const cambiarPagina = (numeroPagina) => {
+  const cambiarPagina = (numeroPagina, nuevaCategoria = categoria) => {
     setPaginaActual(numeroPagina);
+    setCategoria(nuevaCategoria);
+    const categoriaSeleccionada = categoriasInfo.find(cat => cat.nombre === nuevaCategoria);
+    setTotalPaginas(categoriaSeleccionada.totalPaginas);
+
+    navigate("/productos/palabra")
+
+    window.scrollTo({
+      top: 300,
+    })
   }
 
   return (
     <>
-
       <Cabecera />
       <HeroPrincipal />
+      <div className="productos-categoria_select">
+        <p>Categorias: </p>
+        <select onChange={(e) => cambiarPagina(1, e.target.value)}>
+          {categoriasInfo.map((categoria) => (
+            <option key={categoria.nombre} value={categoria.nombre}>
+              {categoria.nombre}
+            </option>
+          ))
+          }
+        </select>
+      </div>
       <div className="productos-contenedor">
-
-        <div className="productos-productos_contenedor">
-          {
-            productos.map((producto, index) => (
-              <CardProducto
-                key={index}
-                producto={producto}
-              />
+        <div className="productos-categoria">
+          <p>Categorias</p>
+          {categoriasInfo.map((cat) => (
+            <Link to="#" key={cat.nombre}
+             onClick={() => cambiarPagina(1, cat.nombre)}
+             className={`productos-categoria_opcion ${categoria === cat.nombre ? 'activo' : ''}`}
+            >
+              {cat.nombre}
+            </Link>
             ))
           }
         </div>
-        <div className="productos-paginacion">
-          {Array.from({ length: totalPaginas }, (_, i) => (
-            <button
-              key={i}
-              onClick={() => cambiarPagina(i + 1)}
-              className={paginaActual === i + 1 ? 'activo' : ''}
-            >
-              {i + 1}
-            </button>
-          ))}
+        <div className="productos-contenedor_distribucion">
+          <div className="productos-productos_contenedor">
+            {
+              productos.map((producto, index) => (
+                <CardProducto
+                  key={index}
+                  producto={producto}
+                />
+              ))
+            }
+          </div>
+          <div className="productos-paginacion">
+            {Array.from({ length: totalPaginas }, (_, i) => (
+              <button
+                key={i}
+                onClick={() => cambiarPagina(i + 1)}
+                className={paginaActual === i + 1 ? 'activo' : ''}
+              >
+                {i + 1}
+              </button>
+            ))}
+          </div>
         </div>
+
 
       </div>
       <Footer />
